a50ecc4294ce2aae9922cb6e28830410ba7246ec
Nico R. Wohlgemuth <nico@lifeisabug.com>
2012-09-22 02:09:21 +0200

    trackbar.pl: cosmetic changes

--- scripts/trackbar.pl
+++ scripts/trackbar.pl
@@ -1,4 +1,22 @@
 # trackbar.pl
+#
+# Track what you read last when switching to a window.
+#
+#    Copyright (C) 2003  Peter Leurs
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # 
 # This little script will do just one thing: it will draw a line each time you
 # switch away from a window. This way, you always know just upto where you've
@@ -167,106 +185,103 @@ Irssi::signal_add(
 
 # terminal resize code inspired by nicklist.pl
 sub sig_terminal_resized {
-	if ($screen_resizing) {
-		# prevent multiple resize_trackbars from running
-		return;
-	}
-	$screen_resizing = 1;
-	Irssi::timeout_add_once(10,\&resize_trackbars,[]);
+   if ($screen_resizing) {
+      # prevent multiple resize_trackbars from running
+      return;
+   }
+   $screen_resizing = 1;
+   Irssi::timeout_add_once(10,\&resize_trackbars,[]);
 }
 
 sub resize_trackbars {
    my $active_win = Irssi::active_win();
-	for my $window (Irssi::windows) {
-		next unless defined $window;
-		my $line = $window->view()->get_bookmark('trackbar');
-		next unless defined $line;
+   for my $window (Irssi::windows) {
+      next unless defined $window;
+      my $line = $window->view()->get_bookmark('trackbar');
+      next unless defined $line;
 
-		# first add new trackbar line, then remove the old one. For some reason 
-		# this works better than removing the old one, then adding a new one
-		$window->print_after($line, MSGLEVEL_NEVER, line($window->{'width'}));
-		my $next = $line->next();
-		$window->view()->set_bookmark('trackbar', $next);
+      # first add new trackbar line, then remove the old one. For some reason 
+      # this works better than removing the old one, then adding a new one
+      $window->print_after($line, MSGLEVEL_NEVER, line($window->{'width'}));
+      my $next = $line->next();
+      $window->view()->set_bookmark('trackbar', $next);
       $window->view()->remove_line($line);
 
       # This hack exists to work around a bug: see IRSSI RESIZE BUG above.
       # Add a line after the trackbar and delete it immediately
       $window->print_after($next, MSGLEVEL_NEVER, line(1));
       $window->view()->remove_line($next->next);
-	}   
-	$active_win->view()->redraw();
-	$screen_resizing = 0;
+   }   
+   $active_win->view()->redraw();
+   $screen_resizing = 0;
 }
 
 Irssi::signal_add('terminal resized' => \&sig_terminal_resized);
 
 sub line {
-    my $width  = shift;
-    my $string = $config{'trackbar_string'};
-    $string = '-' unless defined $string;
+   my $width  = shift;
+   my $string = $config{'trackbar_string'};
+   $string = '-' unless defined $string;
 
-    my $tslen = 0;
+   my $tslen = 0;
 
-    if ($config{'trackbar_timestamp'}) {
-       $tslen = int(1 + length $config{'timestamp_format'});
-    }
+   if ($config{'trackbar_timestamp'}) {
+      $tslen = int(1 + length $config{'timestamp_format'});
+   }
 
-    # There is a bug in irssi's utf-8 handling on config file settings, as you 
-    # can reproduce/see yourself by the following code sniplet:
-    #
-    #   my $quake = pack 'U*', 8364;    # EUR symbol
-    #   Irssi::settings_add_str 'temp', 'temp_foo' => $quake;
-    #   Irssi::print length $quake;
-    #       # prints 1
-    #   Irssi::print length Irssi::settings_get_str 'temp_foo';
-    #       # prints 3
-    #
-    #
-    # Trackbar used to have a workaround, but on recent versions of perl/irssi
-    # it does no longer work.  Therefore, if you want your trackbar to contain
-    # unicode characters, uncomment the line below for a nice full line, or set
-    # the string to whatever char you want.
+   # There is a bug in irssi's utf-8 handling on config file settings, as you 
+   # can reproduce/see yourself by the following code sniplet:
+   #
+   #   my $quake = pack 'U*', 8364;    # EUR symbol
+   #   Irssi::settings_add_str 'temp', 'temp_foo' => $quake;
+   #   Irssi::print length $quake;
+   #       # prints 1
+   #   Irssi::print length Irssi::settings_get_str 'temp_foo';
+   #       # prints 3
+   #
+   #
+   # Trackbar used to have a workaround, but on recent versions of perl/irssi
+   # it does no longer work.  Therefore, if you want your trackbar to contain
+   # unicode characters, uncomment the line below for a nice full line, or set
+   # the string to whatever char you want.
 
-    # 0x2504 and 0x2508 work well too
-    #$string = pack('U*', 0x2500);
+   # 0x2504 and 0x2508 work well too
+   #$string = pack('U*', 0x2500);
 
-    my $length = length $string;
+   my $length = length $string;
 
-    if ($length == 0) {
-        $string = '-';
-        $length = 1;
-    }
+   if ($length == 0) {
+       $string = '-';
+       $length = 1;
+   }
 
-    my $times = $width / $length - $tslen;
-    $times = int(1 + $times) if $times != int($times);
-    $string =~ s/%/%%/g;
+   my $times = $width / $length - $tslen;
+   $times = int(1 + $times) if $times != int($times);
+   $string =~ s/%/%%/g;
 
-    if ($tslen) {
-       # TODO why $config{'timestamp_format'} won't work here?
-       my $ts = strftime(Irssi::settings_get_str('timestamp_format')." ", localtime);
+   if ($tslen) {
+      # why $config{'timestamp_format'} won't work here?
+      my $ts = strftime(Irssi::settings_get_str('timestamp_format')." ", localtime);
 
-       if ($config{'trackbar_timestamp_styled'}) {
-          return $config{'trackbar_style'} . $ts . substr($string x $times, 0, $width);
-       }
-       else
-       {
-          return $ts . $config{'trackbar_style'} . substr($string x $times, 0, $width);
-       }
-    }
-    else
-    {
-       return $config{'trackbar_style'} . substr($string x $times, 0, $width);
-    }
+      if ($config{'trackbar_timestamp_styled'}) {
+         return $config{'trackbar_style'} . $ts . substr($string x $times, 0, $width);
+      } else {
+         return $ts . $config{'trackbar_style'} . substr($string x $times, 0, $width);
+      }
+   }
+   else {
+      return $config{'trackbar_style'} . substr($string x $times, 0, $width);
+   }
 }
 
 # Remove trackbars on upgrade - but this doesn't really work if the scripts are not loaded in the correct order... watch out!
 Irssi::signal_add_first( 'session save' => sub {
-	    for my $window (Irssi::windows) {	
-			next unless defined $window;
-			my $line = $window->view()->get_bookmark('trackbar');
-			$window->view()->remove_line($line) if defined $line;
-	    }
-	}
+      for my $window (Irssi::windows) {   
+         next unless defined $window;
+         my $line = $window->view()->get_bookmark('trackbar');
+         $window->view()->remove_line($line) if defined $line;
+      }
+   }
 );
 
 sub cmd_mark {
